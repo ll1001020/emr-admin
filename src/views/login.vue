776e304b528b1aece1583b87148ede954bbf5e44
ll1001020 <1376992347@qq.com>
2025-02-22 19:16:57 +0800

    modified:   src/views/login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,20 +1,26 @@
 <template>
-		<el-card class="box-card" style="margin: auto;margin-top: 10%;">
 			<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-				<el-form-item label="电话" prop="name">
-					<el-input v-model="ruleForm.name"></el-input>
+				
+				<el-form-item label="电话" prop="phone">
+					<el-input v-model="ruleForm.phone"></el-input>
 				</el-form-item>
 
-				<el-form-item label="密码" prop="desc">
-						<el-input  v-model="ruleForm.desc"></el-input>
+				<el-form-item label="密码" prop="password">
+						<el-input  v-model="ruleForm.password"></el-input>
+				</el-form-item>
+				
+				<el-form-item >
+				    <el-radio-group v-model="ruleForm.resource">
+				      <el-radio type="docotor" label="doctor"></el-radio>
+				      <el-radio type="patient" label="patient"></el-radio>
+				    </el-radio-group>
 				</el-form-item>
 			
 				<el-form-item>
-					<el-button type="primary" @click="submitForm('ruleForm')" :loading="load123">立即创建</el-button>
+					<el-button type="primary" @click="submitForm('ruleForm')" :loading="load123">立即登录</el-button>
 					<el-button @click="resetForm('ruleForm')">重置</el-button>
 				</el-form-item>
 			</el-form>
-		</el-card>
 </template>
 
 <script>
@@ -22,44 +28,55 @@
     data() {
       return {
         ruleForm: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: '',
+          phone: '',
+          password: '',
+		  resource: ''
         },
 		load123:false,
         rules: {
-          name: [
-            { required: true, message: '请输入电话', trigger: 'blur' },
-            { min: 11, max:11, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          phone: [
+            { required: true, message: '填写电话号码', trigger: 'blur' },
+            { min: 11, max:11, message: '长度为 11 个字符', trigger: 'blur' }
           ],
-          desc: [
+          password: [
             { required: true, message: '请填写密码', trigger: 'blur' }
           ]
         }
       };
     },
     methods: {
-      submitForm(formName) {
-		  this.load123 = true;
-        this.$refs[formName].validate((valid) => {
+      submitForm(ruleForm) {
+        this.$refs.ruleForm.validate((valid) => {
+			console.log('表单验证结果:', valid);
+			console.log(this.ruleForm);
           if (valid) {
-            alert('submit!');
-		this.load123 = false;
+			let _this = this;
+			if(_this.ruleForm.resource == 'doctor'){
+				axios.get('http://localhost:8081/doctor/login',{params:_this.ruleForm}).then(function(resp){
+					console.log(resp)
+				})
+				if(resp.data.code == 0){
+					alert('登录成功')
+				}else{
+					alert('登录失败')
+				}
+
+			}else if(_this.ruleForm.resource == 'patient'){
+				axios.get('http://localhost:8081/patient/login',{params:_this.ruleForm}).then(function(resp){
+					console.log(resp)
+				})
+			}
 			
           } else {
-            console.log('error submit!!');
-			this.load123 = false;
+			
+			console.log('error submit!!');
             return false;
+			
           }
         });
       },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
+      resetForm(ruleForm) {
+        this.$refs[ruleForm].resetFields();
       }
     }
   }
@@ -69,14 +86,8 @@
 	.text {
 	    font-size: 14px;
 	  }
-	
-	  .item {
-	    padding: 18px 0;
+	  .demo-ruleForm{
+		padding: 0px;
 	  }
-	
-	  .box-card {
-		  padding-top: 20px;
-	    width: 480px;
-		height: 480px;
-	  }
+	  
 </style>
